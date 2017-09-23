06c8b494b0805aa5d269bacb150cbe5fae5d6acc
Graham Fetterman <graham.fetterman@gmail.com>
2017-09-22 20:13:36 -0500

    Make into package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='zfparse',
+      version='0.1',
+      description='A simple parser for zebra finch vocalizations',
+      url='http://github.com/gfetterman/zfparse',
+      author='Graham Fetterman',
+      author_email='graham.fetterman@gmail.com',
+      license='GPL',
+      packages=['zfparse',],
+      zip_safe=False)
+
